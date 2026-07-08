5604455b188de51c79d5dd88a8c09752b418c44d
League Student <student@jointheleague.org>
2026-07-08 13:16:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/fractal.py
+++ lessons/10_Turtles/10_Welcome/fractal.py
@@ -62,10 +62,12 @@ def fractal_curve(size, depth, sign=1):
         tina.forward(size)
         tina.left(90 * sign)
     else: #draw 2 smaller curves, one flipped
-        fracral_curve(size/math.sqrt(2),depth-1,sign)
+        tina.left(45 * sign)
+        fractal_curve(size/math.sqrt(2),depth-1,sign)
         tina.right(90 * sign)
-        fracral_curve(size/math.sqrt(2),depth-1,-sign)
+        fractal_curve(size/math.sqrt(2),depth-1,-sign)
         tina.left(90 * sign)
+        tina.right(45 * sign)
 
 
 
