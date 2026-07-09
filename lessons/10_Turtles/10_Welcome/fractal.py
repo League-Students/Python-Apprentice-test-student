74def5440570ee86c42a64a8e88fa087c7881232
League Student <student@jointheleague.org>
2026-07-09 12:33:04 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/fractal.py
+++ lessons/10_Turtles/10_Welcome/fractal.py
@@ -93,7 +93,7 @@ def fractal_cool(size,depth,color):
         #tina.end_fill()
   else: #recursive case, draw 4 smaller squares, slightly different colors
       for i in range(4):
-          color
+          color = color
           fractal_cool(size/2,depth-1)
           tina.forward(size)
           tina.left(120)
