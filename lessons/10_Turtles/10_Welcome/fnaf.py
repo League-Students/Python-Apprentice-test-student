cb2231af3e45afba1a246b312448206036d88a15
League Student <student@jointheleague.org>
2026-07-10 13:01:22 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/fnaf.py
+++ lessons/10_Turtles/10_Welcome/fnaf.py
@@ -12,7 +12,10 @@ cam_colors = ["white","blue","red","black","green"]
 
 def show_animatronics(cam_num):
     #tina show
-    if(cam_num == tina_path[tina_progress])
+    if(cam_num == tina_path[tina_progress]):
+        tina.showturtle()
+    else:
+        tina.hideturtle()
 
 def open_cam_1():
     print("cam 1 open")
