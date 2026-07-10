54b768deb50f1615da3f5546103d51efafed82fa
League Student <student@jointheleague.org>
2026-07-10 13:15:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/10_Welcome/fnaf.py
+++ lessons/10_Turtles/10_Welcome/fnaf.py
@@ -53,8 +53,6 @@ screen.onkey(open_cam_4, "4")
 screen.onkey(open_cam_5, "5")
 screen.onkey(exit_cam, "0")
 
-while True:
-    time.
-    tina_progress += 1
+screen.ontimer()
 
 turtle.exitonclick()
